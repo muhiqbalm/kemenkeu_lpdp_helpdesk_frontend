e06dfc9fcbe25300df1e3372c6b823a423c2e16e
Muhammad Iqbal Masykuri <muhammadiqbal2020@mail.ugm.ac.id>
2023-04-20 19:54:51 +0700

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -104,21 +104,23 @@ export default function Dashboard() {
             />
           </div>
           <div className="flex flex-col w-full h-full overflow-y-scroll pb-10 border-l border-abu-muda justify-between">
-            <div className="w-full flex justify-between">
-              <p className="font-bold pt-10 px-20 pb-0 text-lg mb-0">
-                Hasil Pencarian
-              </p>
-              <p className="pt-10 px-20 pb-0 text-lg text-abu mb-0">
-                {filteredResults.length} hasil ditemukan
-              </p>
-            </div>
+            <div>
+              <div className="w-full flex justify-between">
+                <p className="font-bold pt-10 px-20 pb-0 text-lg mb-0">
+                  Hasil Pencarian
+                </p>
+                <p className="pt-10 px-20 pb-0 text-lg text-abu mb-0">
+                  {filteredResults.length} hasil ditemukan
+                </p>
+              </div>
 
-            {currentItems.map((result) => (
-              <SearchResult
-                pertanyaan={result.pertanyaan}
-                jawaban={result.jawaban}
-              />
-            ))}
+              {currentItems.map((result) => (
+                <SearchResult
+                  pertanyaan={result.pertanyaan}
+                  jawaban={result.jawaban}
+                />
+              ))}
+            </div>
 
             <ReactPaginate
               breakLabel="..."
